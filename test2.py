9825a627a4271ec2be79a470891ac9e8510fdb72
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-11-25 18:43:37 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import quote
 
 def scrape_data(url, css_selector):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'lxml')
         selected_elements = soup.select(css_selector)
-
-        # Create a list to store the scraped data
-        scraped_data = [element.text.strip() for element in selected_elements]
-
-        return scraped_data
+        
+        for element in selected_elements:
+            print(element.text.strip())
     else:
-        return f"Failed to fetch data from {url}"
+        print(f"Failed to fetch data from {url}. Status code: {response.status_code}, Content: {response.text}")
 
 # List of URLs to scrape
 urls_to_scrape = [
@@ -24,15 +23,7 @@ urls_to_scrape = [
 css_selector = "#product0 > div:nth-child(8) > span:nth-child(1) > span:nth-child(1)"
 
 # Loop through each URL and scrape data
-scraped_results = []
 for url in urls_to_scrape:
     print(f"Scraping data from: {url}")
-    result = scrape_data(url, css_selector)
-    print(result)
-    scraped_results.extend(result)
-    print("\n" + "=" * 50 + "\n")
-
-# Save the scraped results to a text file
-with open("scraped_data.txt", "w") as file:
-    for item in scraped_results:
-        file.write("%s\n" % item)
+    scrape_data(url, css_selector)
+    print("\n" + "="*50 + "\n")
