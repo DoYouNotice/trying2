b5eaa0b15a2ae34f6a77a75a46ca713b0259c2d0
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-11-25 20:40:55 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,8 +16,51 @@ def scrape_data(url, css_selector, headers):
 
 # List of URLs to scrape
 urls_to_scrape = [
-    "https://geizhals.de/?cat=gra16_512&xf=9816_03+05+17+-+RTX+4080&asuch=&bpmin=&bpmax=&v=e&hloc=at&hloc=de&plz=&dist=&mail=&t=v&sort=p&bl1_id=30",
-    "https://geizhals.de/?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4070+Ti"
+
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4090"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4080"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4070+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4070"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4060+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+17+-+RTX+4060"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3090+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3090"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3080+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3080"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3070+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3070"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3060+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3060"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+16+-+RTX+3050"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+15+-+RTX+2080"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+15+-+RTX+2060+SUPER"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+05+15+-+RTX+2060"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+04+15+-+GTX+1660+Ti"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+04+15+-+GTX+1660+SUPER"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+04+15+-+GTX+1660"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+04+15+-+GTX+1650+SUPER"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+04+15+-+GTX+1650"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_03+04+15+-+GTX+1630"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+14+-+RX+7900+XTX"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+14+-+RX+7900+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+14+-+RX+7800+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+14+-+RX+7700+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+14+-+RX+7600"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6950+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6900+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6800+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6800"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6750+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6700+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6700"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6650+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6600+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6600"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6500+XT"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+12+-+RX+6400"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+10+-+RX+5700"
+"https://?cat=gra16_512&v=e&hloc=at&hloc=de&t=v&sort=p&bl1_id=30&xf=9816_02+04+10+-+RX+5600+XT"
+
 ]
 
 # CSS selector to extract data
